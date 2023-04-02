2ceb4749bb656f12345629b1f177e555835050e7
wahidari <wahiid.ari@gmail.com>
2023-04-02 23:43:03 +0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
+const nextConfig = {
+  env: {
+    // API_URL: "http://localhost:3000",
+    API_URL: "https://nextra-v2x.vercel.app",
+    BASE_URL: "https://nextra-v2x.vercel.app",
+    SITE_URL: "nextra-v2x.vercel.app",
+  }
+};
+
+module.exports = withNextra(nextConfig)
